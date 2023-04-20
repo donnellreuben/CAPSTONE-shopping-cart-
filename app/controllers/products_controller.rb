ca76d1fea4ec80bd3e2ae5b937c58218a24853bd
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-20 14:29:26 -0500

    Added CRUD to Products Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,54 @@
 class ProductsController < ApplicationController
+
+# INDEX
   def index
+    @products = Product.all
   end
 
+# SHOW
   def show
+    @product = Product.find(params[:id])
   end
 
+# NEW
   def new
+    @product = Product.new
   end
 
+# CREATE
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
+# UPDATE
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
+# DELETE
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
   end
 end
