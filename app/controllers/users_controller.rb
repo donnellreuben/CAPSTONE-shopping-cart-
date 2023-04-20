51c2767aefb1c22e4366f8d860f2e8735765d5e6
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-20 14:31:50 -0500

    Added CRUD to Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+private
 
-  def update
-  end
-
-  def destroy
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
+
