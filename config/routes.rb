7fb383e1c15418400e67c89c03b45b37d973b198
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-20 13:41:33 -0500

    fixed CartedProduct Model to include product_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-  get 'carted_products/index'
-  get 'carted_products/show'
-  get 'carted_products/new'
-  get 'carted_products/create'
-  get 'carted_products/edit'
-  get 'carted_products/update'
-  get 'carted_products/destroy'
-  get 'orders/index'
-  get 'orders/show'
-  get 'orders/new'
-  get 'orders/create'
-  get 'orders/edit'
-  get 'orders/update'
-  get 'orders/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "products#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products
+  resources :orders, except: [:destroy]
+  resources :carted_products, only: [:index, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  get "/signup" => "users#new"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 end
