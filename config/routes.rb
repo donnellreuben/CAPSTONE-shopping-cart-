3448d07c36d78d6416f9edf503bb627585dd2394
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-20 11:44:11 -0500

    Generated new controller, Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get 'carted_products/index'
   get 'carted_products/show'
   get 'carted_products/new'
