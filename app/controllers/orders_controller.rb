158d76b6ee467ed1a96ae2c292ddd24319e7900b
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-20 14:30:07 -0500

    Added CRUD to Orders Controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @orders = current_user.orders
   end
 
   def show
+    @order = current_user.orders.find(params[:id])
   end
 
   def new
+    @order = Order.new
   end
 
   def create
-  end
-
-  def edit
+    @order = Order.new(
+      user_id: current_user.id,
+      status: "carted"
+    )
+    if @order.save
+      redirect_to carted_products_path
+    else
+      render :new
+    end
   end
 
   def update
+    @order = current_user.orders.find(params[:id])
+    @order.update(order_params)
+    redirect_to order_path(@order)
   end
 
-  def destroy
+private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
+
